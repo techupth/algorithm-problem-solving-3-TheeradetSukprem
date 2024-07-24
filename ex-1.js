e83a1a101b629102878408cd6f51f4a77562cee0
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-07-24 15:05:19 +0700

    feat:complete problem soving 3

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,22 @@
+let customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"];
+
+
 function sortCustomerName(customers) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < customers.length - 1; i++) {
+    console.log(customers[i]);
+    for (let j = 0; j < customers.length - 1 - i; j++) {
+      if (customers[j] > customers[j + 1]) {
+        console.log(customers[j]);
+        let temp = customers[j];
+        console.log(customers);
+        customers[j] = customers[j + 1];
+        customers[j + 1] = temp;
+        console.log(customers); 
+      }
+    }
+  }
+  return customers
 }
 
-// ตอบคำถามตรงนี้จ้า
+console.log(sortCustomerName(customers));
+// ตอบคำถามตรงนี้จ้า Big o เป็น n ^ 2 เพราะว่ามี for loop ซ้อนกันทำให้ทบกัน 
